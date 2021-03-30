b37d0a9209e2e2b2f9256858535c68e0bed57d86
JessyMe <jessy.mercer@gmail.com>
2021-03-30 13:01:23 +0200

    validation ok v1

--- src/components/User/UserRegisterForm.vue
+++ src/components/User/UserRegisterForm.vue
@@ -1,27 +1,27 @@
 <template>
     <v-container fluid>
-        <v-form @submit="submitForm" ref="form">
+        <v-form @submit.prevent=submitForm ref="form" novalidate>
           <v-container fluid>
-            <v-text-field label="Email" rounded color="primary" outlined type="email" v-model="email"></v-text-field>
+            <v-text-field label="Email" rounded color="primary" outlined type="email" v-model="form.email"></v-text-field>
           </v-container>
           <v-container fluid>
-            <v-text-field  type="password" label="Mot de passe" v-model.lazy="password" outlined rounded color="primary"></v-text-field>
+            <v-text-field  type="password" label="Mot de passe" v-model.lazy="form.password" outlined rounded color="primary"></v-text-field>
           </v-container>
           <v-container fluid>
-            <v-text-field  type="text" label="Pseudo" v-model.lazy="username" outlined rounded color="primary"></v-text-field>
+            <v-text-field  type="text" label="Pseudo" v-model.lazy="form.username" outlined rounded color="primary"></v-text-field>
           </v-container>
           <v-container fluid>
-            <v-text-field  type="text" label="Prénom" v-model.lazy="name" outlined rounded color="primary"></v-text-field>
+            <v-text-field  type="text" label="Prénom" v-model.lazy="form.name" outlined rounded color="primary"></v-text-field>
           </v-container>
           <v-container fluid>
-            <v-text-field type="text" label="Nom" v-model.lazy="surname" outlined rounded color="primary"></v-text-field>
+            <v-text-field type="text" label="Nom" v-model.lazy="form.surname" outlined rounded color="primary"></v-text-field>
           </v-container>
           <v-container fluid>
-          <v-btn submit color="orange">Submit</v-btn>
-          <v-btn @click="resetForm" color="grey">Cancel</v-btn>
+          <v-btn submit type="submit" color="orange">Submit</v-btn>
+          <v-btn color="grey">Cancel</v-btn>
           </v-container>
         </v-form>
-        <pre>{{$data}}</pre>
+        <pre>{{$data.form}}</pre>
     </v-container>
 </template>
 
@@ -30,25 +30,67 @@ export default ({
   name: 'user-register-form',
   data () {
     return {
-      email: '',
-      password: '',
-      username: '',
-      name: '',
-      surname: ''
+      form: {
+        email: '',
+        password: '',
+        username: '',
+        name: '',
+        surname: ''
+      },
+      errors: []
     }
   },
   methods: {
-    submitForm (e) {
-      e.preventDefault()
-      console.log(this.data)
+    validEmail () {
+      const emailregex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if (!this.form.email) {
+        this.errors.push("Oups, l'email n'est pas renseigné !")
+        return false
+      }
+      if (this.form.email && !emailregex.test(this.form.email)) {
+        this.errors.push('L\'email ne correspond pas au format demandé')
+        return false
+      }
     },
-    reset () {
-      this.$refs.form.reset()
+    validPassword () {
+      const passwordregex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
+      if (!this.form.password) {
+        this.errors.push("Oups, le mot de passe n'est pas renseigné !")
+        return false
+      }
+      if (this.form.password && !passwordregex.test(this.form.password)) {
+        this.errors.push('Le mot de passe ne correspond pas au format demandé : minimum 8 caractères, une majuscule, une minuscule, un nombre et un caractèrs spécial')
+        return false
+      }
+    },
+    validUsername () {
+
+    },
+    validTextField () {
+      if (this.form.surname && this.form.name) {
+        return true
+      } else {
+        this.errors.push('Tous les champs doivent être renseignés')
+        return false
+      }
+    },
+    validForm () {
+      this.errors = []
+      this.validEmail()
+      this.validPassword()
+      this.validTextField()
+    },
+    submitForm () {
+      if (this.validForm()) {
+        console.log(this.form)
+      } else {
+        console.log(this.errors)
+      }
     }
   }
 })
 
 </script>
 
-<style>
+<style scoped>
 </style>
